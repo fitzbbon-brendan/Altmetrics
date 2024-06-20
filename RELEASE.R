7b94510bf03abd31c54b3932d76087fa305de720
Alex Koiter <akoiter@gmail.com>
2024-06-20 09:24:21 -0500

    Prep for data backup

--- RELEASE.R
+++ RELEASE.R
@@ -1,3 +1,9 @@
 
 # Re-Build the README
 devtools::build_readme()
+
+
+# Update the MB Quarters csv file via piggyback
+# - Download the file by hand if necessary, and store
+piggyback::pb_release_create(repo = "alex-koiter/mbquartR", tag = "data-backup")
+piggyback::pb_upload("DATA_TO_UPLOAD/mb_quarters.csv", repo = "alex-koiter/mbquartR")
